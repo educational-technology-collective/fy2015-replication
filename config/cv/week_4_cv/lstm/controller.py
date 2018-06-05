ea57f4a7a0d76cf4ffb3df5157d403644b590ba3
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-06-04 20:44:45 -0400

    initial commit of config files and controller scripts

---
+++ config/cv/week_4_cv/lstm/controller.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2018 The Regents of the University of Michigan
+# and the University of Pennsylvania
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+A test script to replicate Mi and Yeung (2015) using the MORF 2.0 architecture.
+"""
+
+from morf.workflow.extract import fork_features
+from morf.workflow.cross_validation import create_course_folds, cross_validate_course
+from morf.workflow.evaluate import evaluate_cv_course
+
+fork_features("dl-replication-week4-lr-cv")
+create_course_folds(label_type="dropout")
+cross_validate_course(label_type="dropout")
+evaluate_cv_course(label_type="dropout")
